d05cf7e147f72ea0f6b8df1f8861a1e8a4e7c3b7
MHAE391 <modatherelgohary391@gmail.com>
2022-11-11 13:18:16 +0200

    NASA

--- app/src/main/java/com/m391/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/m391/asteroidradar/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.m391.asteroidradar
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.work.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        delayedInit()
         setContentView(R.layout.activity_main)
+
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun delayedInit() {
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(true)
+            .setRequiresBatteryNotLow(true)
+            .apply {
+                setRequiresDeviceIdle(true)
+            }.build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
